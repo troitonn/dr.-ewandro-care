b05386b1062ec1e602388a378737f3ff5cf50e5c
troitonn <150198515+troitonn@users.noreply.github.com>
2026-03-03 09:19:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADICIONE ESTA LINHA ABAIXO
+  base: "/drewandro/", 
   server: {
     host: "::",
     port: 8080,
