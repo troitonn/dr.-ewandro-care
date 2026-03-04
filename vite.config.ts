5245eb43c2ab8cd14fd962f6839ad00165092df1
troitonn <150198515+troitonn@users.noreply.github.com>
2026-03-04 09:59:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/drewandro/",
+export default defineConfig(() => ({
+  base: process.env.VITE_BASE || "/",
 
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    process.env.NODE_ENV === "development" && componentTagger(),
   ].filter(Boolean),
 
   resolve: {
